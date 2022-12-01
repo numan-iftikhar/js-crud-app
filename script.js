5f2b740b7e44057defaaafd5de065b67a26f92a5
Numan Iftikhar <numaniftikhar300@gmail.com>
2022-12-01 10:06:22 +0500

    finish project

--- script.js
+++ script.js
@@ -16,22 +16,73 @@ function showAlert(msg, color) {
   }, 3000);
 }
 
+// Add Data
+document.querySelector("#student-form").addEventListener("submit", (e) => {
+  e.preventDefault();
 
+  const firstName = document.querySelector("#first-name").value;
+  const lastName = document.querySelector("#last-name").value;
+  const rollNo = document.querySelector("#roll-no").value;
+
+  // Validate Data
+  if (firstName === "" || lastName === "" || rollNo === "") {
+    showAlert("Please! Fill the form first!!", "warning");
+  } else {
+    if (selectedRow === null) {
+      const list = document.querySelector("#student-list");
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+          <td>${firstName}</td>
+          <td>${lastName}</td>
+          <td>${rollNo}</td>
+          <td>
+             <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
+             <a href="#" class="btn btn-danger btn-sm delete">Delete</a>
+          </td>
+        `;
+
+      // Add new row to table
+      list.appendChild(row);
+      selectedRow = null;
+      showAlert("Student Added Successfully", "success");
+    } else {
+      selectedRow.children[0].textContent = firstName;
+      selectedRow.children[1].textContent = lastName;
+      selectedRow.children[2].textContent = rollNo;
+    }
+
+    clearAllFields();
+  }
+});
+
+// Edit Data
+document.querySelector("#student-list").addEventListener("click", (e) => {
+  target = e.target;
+  if (target.classList.contains("edit")) {
+    selectedRow = target.parentElement.parentElement;
+    document.querySelector("#first-name").value =
+      selectedRow.children[0].textContent;
+    document.querySelector("#last-name").value =
+      selectedRow.children[1].textContent;
+    document.querySelector("#roll-no").value =
+      selectedRow.children[2].textContent;
+  }
+});
 
 // Clear all fields
 function clearAllFields() {
-    document.querySelector("#first-name").value = "";
-    document.querySelector("#last-name").value = "";
-    document.querySelector("#roll-no").value = "";
+  document.querySelector("#first-name").value = "";
+  document.querySelector("#last-name").value = "";
+  document.querySelector("#roll-no").value = "";
 }
 
-
 // Delete Data
-document.querySelector("#student-list").addEventListener("click", (e)=>{
+document.querySelector("#student-list").addEventListener("click", (e) => {
   target = e.target;
-  
-  if (target.classList.contains('delete')) {
-    target.parentElement.parentElement.remove()
-    showAlert("Data Deleted Successfully", "danger")
+
+  if (target.classList.contains("delete")) {
+    target.parentElement.parentElement.remove();
+    showAlert("Data Deleted Successfully", "danger");
   }
-})
+});
